5f9c0f250c5b369264ecaaa6005cad004d6ec02f
Rabinagurung <rabinagurung347@gmail.com>
2025-01-16 23:17:52 -0500

    Tailwind CSS Setup & Theming

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 const Home = () => {
   return (
     <div>
-      <h1 className="text-3xl text-violet-700 font-black">
+      <h1 className="h1-bold background-light850_dark100">
         Welcome to the world of Next.js
       </h1>
     </div>
